d71e1b1ffc7b674c3f7ecace2e30f309c84d5054
Souvik Ghosh <96991068+Rongho@users.noreply.github.com>
2023-08-17 03:48:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const server=express();
 
-
+server.use("/",(req,res)=>{
+res.send("Hi its started")  
+})
 server.listen(8000,()=>{
     console.log("server started")
 })
